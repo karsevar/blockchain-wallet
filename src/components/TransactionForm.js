70a70bfdfa5e76ad64d93db08ecaa343fbe51fec
masonkarsevar <masonkarsevar@gmail.com>
2020-04-09 13:28:43 -0700

    basic transaction form finished. WIP: axios.post request functionality

--- src/components/TransactionForm.js
+++ src/components/TransactionForm.js
@@ -1,9 +1,49 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 function TransactionForm(props) {
+  const { currentUserId } = props;
+
+  const [newTransaction, setNewTransaction] = useState({
+    sender: currentUserId,
+    recipient: "",
+    amount: "",
+  });
+
+  const handleChange = (event) => {
+    setNewTransaction({
+      ...newTransaction,
+      [event.target.name]: event.target.value,
+    });
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log("submitting following", newTransaction);
+    setNewTransaction({ sender: currentUserId, recipient: "", amount: "" });
+  };
   return (
     <div className='transaction-form-container'>
-      <p>Transaction form Here!</p>
+      <form onSubmit={handleSubmit}>
+        <input
+          placeholder='sender'
+          value={newTransaction.sender}
+          name='sender'
+          onChange={handleChange}
+        />
+        <input
+          placeholder='recipient'
+          value={newTransaction.recipient}
+          name='recipient'
+          onChange={handleChange}
+        />
+        <input
+          placeholder='amount'
+          value={newTransaction.amount}
+          name='amount'
+          onChange={handleChange}
+        />
+        <button type='submit'>Submit Transaction</button>
+      </form>
     </div>
   );
 }
